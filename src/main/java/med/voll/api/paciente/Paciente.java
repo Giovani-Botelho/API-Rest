43e40cc1f4e6d82a72372b96a859d7490c1e7a84
Giovani-Botelho <giibotelho01@gmail.com>
2025-02-12 10:23:30 -0300

    Adição de funcionalidades

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,76 @@
+package med.voll.api.paciente;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Boolean ativo;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente (){}
+
+    public Paciente(DadosCadastroPaciente dados){
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.telefone = dados.telefone();
+        this.email = dados.email();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null)
+            this.nome = dados.nome();
+
+        if (dados.telefone() != null)
+            this.telefone = dados.telefone();
+
+        if (dados.endereco() != null)
+            endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void inativar() {
+        this.ativo = false;
+    }
+}
